6c7632d077d8e7777de741979a3d67705d4701f2
unknown <lpresotto@frba.utn.edu.ar>
2024-05-14 23:47:15 -0300

    Agregada a practica de haskell de fold: funcion fibo1, la fibonacci tecnicamente con fold mas trucha que existe

--- Haskell-Practica/PdeP/Otras Practicas/Practica Fold.hs
+++ Haskell-Practica/PdeP/Otras Practicas/Practica Fold.hs
@@ -4,3 +4,10 @@ sumar = foldr (+) 0
 
 sumarcuadrados :: (Num a) => [a] -> a
 sumarcuadrados = foldr (\x -> \y -> x^2+y) 0
+
+fibo1 :: Integer -> Integer
+fibo1 x 
+    |x==1 = 1
+    |x==2 = 1
+    |x>2 = foldl (+) 0 (map fibo1 [x-2..x-1])
+
