bf95afea84cc102aafb6a2e298480b6a74c1f237
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-06-10 13:39:35 +0200

    Termino

--- Haskell-Practica/PdeP/Evaluaciones-de-practica/Carreras/Carreras.hs
+++ Haskell-Practica/PdeP/Evaluaciones-de-practica/Carreras/Carreras.hs
@@ -59,7 +59,7 @@ miguelitos efectoMiguelito auto = afectarALosQueCumplen (gana auto) (reducirVelo
 
 jetPack :: Int -> PowerUp
 jetPack tiempo auto = afectarALosQueCumplen (== auto) (modificarVelocidad (flip div 2) . correr tiempo . modificarVelocidad (*2))
-
+--4a
 type Evento = Carrera -> Carrera
 type ListaDePosiciones = [(Int, Color)]
 
@@ -71,5 +71,37 @@ mostrarPosicionAuto auto carrera = (puesto auto carrera, color auto)
 
 simularCarrera :: Carrera -> [Evento] -> ListaDePosiciones
 simularCarrera estadoInicial eventos = mostrarPosicionesCarrera $ foldl (flip ($)) estadoInicial eventos
+--4b
+correrTodos :: Int -> Evento
+correrTodos tiempo = map (correr tiempo)
+
+primerAutoConColor :: Color -> Carrera -> Auto
+primerAutoConColor colorBuscado = head . filter ((==colorBuscado) . color) 
+
+usaPowerUp :: Color -> PowerUp -> Evento
+usaPowerUp colorDeAuto powerup carrera = powerup (primerAutoConColor colorDeAuto carrera) carrera
+
+rojo :: Auto
+rojo = Auto "Rojo" 120 0
+
+negro :: Auto
+negro = Auto "Negro" 120 0
+
+blanco :: Auto
+blanco = Auto "Blanco" 120 0
+
+azul :: Auto
+azul = Auto "Azul" 120 0
+
+eventos :: [Evento]
+eventos = [correrTodos 30, usaPowerUp "Azul" $ jetPack 3, usaPowerUp "Blanco" terremoto, correrTodos 40, usaPowerUp "Blanco" $ miguelitos 20, usaPowerUp "Negro" $ jetPack 6, correrTodos 10]
+
+-- Seria posible, ya que se podía plantar el powerUp como "serImpactadoPorMisil", de modo que luego cuando se utilize la funcion de orden superior usaPowerUp, se la llame directamente al objetivo del misil
+-- Tambien es posible, si se desea que el misil lo active algun auto en particular, que la funcion misilTeledirigido utilize la funcion afectarALosQueCumplen tomando de primer parametro ((== colorApuntado) . color)
+
+-- La funcion vaTranquilo solo sería evaluable si dada una carrera de infinitos autos, algun auto va cerca, haciendo que la funcion devuelva False dado que deja de la funcion any deja de evaluar la lista infinita al encontrar un resultado falso, como comsecuencia de que haskell utiliza lazy evaluation
+-- Si ningun auto de la lista infinita va cerca, la funcion seguirá evaluando autos hasta que la computadora se quede sin memoria
+
+-- La funcion puesto nunca deja de evaluar, puesto que utiliza la funcion length sobre una lista filtrada y no es posible que la funcion length deje de evaluar los elementos de una lista antes de terminar.
 
 
