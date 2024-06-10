a03eeef52c757ff259851efe5df0b95f484fc99e
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-06-10 13:03:49 +0200

    Punto 4a

--- Haskell-Practica/PdeP/Evaluaciones-de-practica/Carreras/Carreras.hs
+++ Haskell-Practica/PdeP/Evaluaciones-de-practica/Carreras/Carreras.hs
@@ -26,7 +26,50 @@ puesto auto = (1+) . length . filter ((/= color auto) . color) . filter (not . g
 mario :: Auto
 mario = Auto "Rojo" 30 14
 
+carreraDeEjemplo :: [Auto]
 carreraDeEjemplo = [Auto "Verde" 2 6, Auto "Azul" 205 3, Auto "Naranja" 1 1000, mario]
 
+--12:30
+--Punto 2 
+modificarDistancia :: (Int -> Int) -> Auto -> Auto
+modificarDistancia f auto = auto{distancia = f . distancia $ auto}
+modificarVelocidad :: (Int -> Int) -> Auto -> Auto
+modificarVelocidad f auto = auto{velocidad = f . velocidad $ auto}
+
+correr :: Int -> Auto -> Auto
+correr tiempo auto = modificarDistancia (+ tiempo * velocidad auto) auto
+
+reducirVelocidad :: Int -> Auto -> Auto
+reducirVelocidad reduccion auto 
+    |velocidad auto > reduccion = modificarVelocidad (subtract reduccion) auto
+    |otherwise = modificarVelocidad (const 0) auto
+
+-- Punto 3 
+
+afectarALosQueCumplen :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+afectarALosQueCumplen criterio efecto lista = (map efecto . filter criterio) lista ++ filter (not.criterio) lista
+
+type PowerUp = Auto ->  Carrera -> Carrera
+
+terremoto :: PowerUp
+terremoto auto = afectarALosQueCumplen (estaCerca auto) (reducirVelocidad 50)
+
+miguelitos :: Int -> PowerUp
+miguelitos efectoMiguelito auto = afectarALosQueCumplen (gana auto) (reducirVelocidad efectoMiguelito)
+
+jetPack :: Int -> PowerUp
+jetPack tiempo auto = afectarALosQueCumplen (== auto) (modificarVelocidad (flip div 2) . correr tiempo . modificarVelocidad (*2))
+
+type Evento = Carrera -> Carrera
+type ListaDePosiciones = [(Int, Color)]
+
+mostrarPosicionesCarrera :: Carrera -> ListaDePosiciones
+mostrarPosicionesCarrera carrera = map (flip mostrarPosicionAuto carrera) carrera
+
+mostrarPosicionAuto :: Auto -> Carrera -> (Int, Color)
+mostrarPosicionAuto auto carrera = (puesto auto carrera, color auto)
+
+simularCarrera :: Carrera -> [Evento] -> ListaDePosiciones
+simularCarrera estadoInicial eventos = mostrarPosicionesCarrera $ foldl (flip ($)) estadoInicial eventos
 
 
