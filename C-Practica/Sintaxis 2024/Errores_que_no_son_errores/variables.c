d2de6c52351b7423cfa5064f74956eaff925528a
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-10-27 10:37:22 -0300

    sintaxis errores

---
+++ C-Practica/Sintaxis 2024/Errores_que_no_son_errores/variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int scanf = 5523;
+    printf(" --> %d \n",scanf);
+    int pi = 3.14;
+    printf(" - %d - %f - %c - \n", pi, pi, pi, pi);
+    int a = 5.86;
+    int b = a + pi;
+    printf("%d", b);
+    b+=0.99+0.01;
+    printf("%d", b);
+    return 0;
+}
+
+void pi(){
+    return;
+}
