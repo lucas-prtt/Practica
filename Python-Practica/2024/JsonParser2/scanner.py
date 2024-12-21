98ebf03287b321038dcac04e608f74da5f2b4ce9
lucas-prtt <lpresotto@frba.utn.edu.ar>
2024-12-21 11:32:09 -0300

    Scanner palabra por palabra

--- Python-Practica/2024/JsonParser2/scanner.py
+++ Python-Practica/2024/JsonParser2/scanner.py
@@ -1,15 +1,14 @@
 class Token:
     def __init__(self, type, val):
-        setattr(self, "tipo", type)
-        setattr(self, "valor", val)
+        setattr(self, "type", type)
+        setattr(self, "val", val)
     def __repr__(self):
-        return "<Objeto Token: Tipo: {}, Valor: {}>".format(self.tipo, self.valor)
+        return "<Objeto Token: Tipo: {}, Valor: {}>".format(self.type, self.val)
 
 
 def scan(string):
     index = 0
-    tokens = []
-
+    token = None
     def scanNumber(string, index):
         start = index
         while index < len(string) and string[index] in "0123456789.-":
@@ -25,41 +24,39 @@ def scan(string):
             index +=1
         index += 1 # Skip de <"> 
         return string[start+1:index-1], index-start
-                
-    def newToken(type, val):
-        tokens.append(Token(type, val))
         
-    while index < len(string):
-        match(string[index]):
-            case "{":
-                index+=1
-                newToken("{", None)
-            case "}":
-                index+=1
-                newToken("}", None)
-            case " ":
-                index+=1
-            case ":":
-                index+=1
-                newToken("}", None)
-            case '"':
-                value, long = scanString(string, index)
+
+    while string[index] == " ":
+        index += 1
+
+    match(string[index]):
+        case "{":
+            index+=1
+            token = Token("{", None)
+        case "}":
+            index+=1
+            token = Token("}", None)
+        case ":":
+            index+=1
+            token = Token(":", None)
+        case '"':
+            value, long = scanString(string, index)
+            index += long
+            token = Token("String", value)
+        case '[':
+            index+=1
+            token = Token("[", None)
+        case ']':
+            index+=1
+            token = Token("]", None)
+        case ',':
+            index+=1
+            token = Token(",", None)
+        case _:
+            if string[index] in "0123456789.-":
+                value, long = scanNumber(string, index)
                 index += long
-                newToken("String", value)
-            case '[':
-                index+=1
-                newToken("[", None)
-            case ']':
-                index+=1
-                newToken("]", None)
-            case ',':
-                index+=1
-                newToken(",", None)
-            case _:
-                if string[index] in "0123456789.-":
-                    value, long = scanNumber(string, index)
-                    index += long
-                    newToken("Number", value)
-                else:
-                    raise Exception("Error de scanner en indice del string: {} \n".format(index))
-    return tokens
+                token = Token("Number", value)
+            else:
+                raise Exception("Error de scanner en indice del string: {} \n".format(index))
+    return token, string[index:]
