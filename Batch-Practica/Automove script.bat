60dc060a4826d7e0f9234a32653e41f14a7e53d5
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-01-22 20:33:14 -0300

    js practica y batch mover archivos

---
+++ Batch-Practica/Automove script.bat
@@ -0,0 +1,4 @@
+set file="C:\Users\Lucas\Desktop\folder2"
+set to="C:\Users\Lucas\Desktop\folder1"
+
+mv %file% %to%
---
+++ Javascript-Html-Practica/Pruebas-iniciales/Pruebas/code.js
@@ -0,0 +1,15 @@
+var boton1 = document.getElementById("boton1")
+var boton2 = document.getElementById("boton2")
+
+
+funcion1 = function () {
+    alert("apretado el boton 1");
+};
+
+
+funcion2 = function () {
+    alert("apretado el boton 2");
+};
+boton2.onclick = funcion2   
+
+// boton 1 linkeado desde el html, boton 2 linkeado a traves de codigo
