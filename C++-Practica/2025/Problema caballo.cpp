d217babee4e7308be47962ce960bd968de829896
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-04-07 12:55:41 -0300

    copiarmatriz

--- C++-Practica/2025/Problema caballo.cpp
+++ C++-Practica/2025/Problema caballo.cpp
@@ -11,9 +11,28 @@ struct nodo_tablero{
     int tablero[8][8] = {0};
     nodo_tablero *movimientos[8] = {NULL};
 };
+
+void copiarMatriz(int origen[8][8], int destino[8][8]){
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            destino[i][j] = origen[i][j];
+        }
+    }
+}
+
+void mostrarTablero(int tablero[8][8]){
+    for(int i=0; i<8; i++){
+        for(int j=0; j<8; j++){
+            cout<<tablero[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+}
+
+
 bool buscar(int paso, int x , int y , nodo_tablero * raiz){
     if(paso == 65){
-
+        
         return true;
     }
     if(x<0 or x>=8 or y<0 or y>=8 or raiz->tablero[x][y] != 0){
@@ -22,15 +41,19 @@ bool buscar(int paso, int x , int y , nodo_tablero * raiz){
     }
     raiz->tablero[x][y]=paso;
     bool valido = false;
+    cout <<"<a<"<<x<<y<<">a>";
+    mostrarTablero(raiz->tablero);
     for(int i=0; i<8 and !valido; i++){
-    raiz->movimientos[i] = new nodo_tablero;
-    bool analizadoValido = buscar(paso+1, x+posibilidades[i][0], y+posibilidades[i][1], raiz->movimientos[i]);
-    valido = valido or analizadoValido;
-    if(!analizadoValido){
-        delete raiz->movimientos[i];
-        raiz->movimientos[i] = NULL;
+        cout<<i;
+        raiz->movimientos[i] = new nodo_tablero;
+        bool analizadoValido = buscar(paso+1, x+posibilidades[i][0], y+posibilidades[i][1], raiz->movimientos[i]);
+        valido = valido or analizadoValido;
+        if(!analizadoValido){
+            delete raiz->movimientos[i];
+            raiz->movimientos[i] = NULL;
+        }
     }
-    }
+    cout<<"b";
     return valido;
 }
 nodo_tablero * hallarEncontrado(int paso, nodo_tablero * raiz){
@@ -54,23 +77,12 @@ void hallarMovimientos(int x_inicial, int y_inicial, int tablero_encontrado[8][8
         cout<<"Encontrado!\n";
         encontrado = hallarEncontrado(1, arbol);
     }
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            tablero_encontrado[i][j] = encontrado->tablero[i][j];
-        }
-    }
+    copiarMatriz(encontrado->tablero, tablero_encontrado);
+
     delete arbol;
     return;
 }
 
-void mostrarTablero(int tablero[8][8]){
-    for(int i=0; i<8; i++){
-        for(int j=0; j<8; j++){
-            cout<<tablero[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-}
 
 int main(){
     int x_inicial , y_inicial;
